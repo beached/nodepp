69062167a114760b56bd819902c092a3091c7fcd
Darrell Wright <dwright-git@dawdevel.ca>
2014-11-26 23:39:30 -0500

    Fixed const order so it reads r2l and moved ends_with above callers

--- string.h
+++ string.h
@@ -64,7 +64,7 @@ namespace daw {
 		}
 
 		template<typename Delim>
-		std::vector<details::string_t> &split( const details::string_t &s, Delim delim, std::vector<details::string_t> &elems ) {
+		std::vector<details::string_t> &split( details::string_t const &s, Delim delim, std::vector<details::string_t> &elems ) {
 			std::stringstream ss( s );
 			details::string_t item;
 			while( std::getline( ss, item, delim ) ) {
@@ -74,42 +74,48 @@ namespace daw {
 		}
 
 		template<typename Delim>
-		std::vector<details::string_t> split( const details::string_t &s, Delim delim ) {
+		std::vector<details::string_t> split( details::string_t const &s, Delim delim ) {
 			std::vector<details::string_t> elems;
 			split( s, delim, elems );
 			return elems;
 		}
 
 		template<typename Arg>
-		details::string_t to_string( const Arg& arg ) {
+		details::string_t to_string( Arg const& arg ) {
 			return boost::lexical_cast<details::string_t>(arg);
 		}
 
 		namespace {
 			template<typename Arg>
-			auto string_join( const Arg& arg ) -> decltype(to_string( arg )) {
+			auto string_join( Arg const & arg ) -> decltype(to_string( arg )) {
 				return to_string( arg );
 			}
 		}
 
 		template<typename Arg1, typename Arg2>
-		auto string_join( const Arg1& arg1, const Arg2& arg2 ) -> decltype(to_string( arg1 ) + to_string( arg2 )) {
+		auto string_join( Arg1 const & arg1, Arg2 const & arg2 ) -> decltype(to_string( arg1 ) + to_string( arg2 )) {
 			return to_string( arg1 ) + to_string( arg2 );
 		}
 
 		template<typename Arg1, typename Arg2, typename... Args>
-		auto string_join( const Arg1& arg1, const Arg2& arg2, const Args & ... args ) -> decltype(string_join( string_join( arg1, arg2 ), string_join( args... ) )) {
+		auto string_join( Arg1 const & arg1, Arg2 const & arg2, Args const & ... args ) -> decltype(string_join( string_join( arg1, arg2 ), string_join( args... ) )) {
 			return string_join( string_join( arg1, arg2 ), string_join( args... ) );
+		}
+
+		template<typename StringType, typename Ending>
+		bool ends_with( StringType const & src, Ending const & ending ) {
+			const static auto func = ends_with_t<StringType>( );
+			return func( src, ending );
 		}
 
 		//////////////////////////////////////////////////////////////////////////
 		// Summary: takes a format string like "{0} {1} {2}" and in this case 3 parameters
 		template<typename Arg, typename... Args>
 		details::string_t string_format( details::string_t format, Arg arg, Args... args ) {
-			const std::regex reg( R"^(\{(\d+(:\d)*)\})^" );
+			std::regex const reg( R"^(\{(\d+(:\d)*)\})^" );
 			static std::stringstream ss;
 			clear( ss );
-			const auto arguments = details::unknowns_to_string( arg, args... );
+			auto const arguments = details::unknowns_to_string( arg, args... );
 			std::smatch sm;
 			while( std::regex_search( format, sm, reg ) ) {
 				auto const & prefix = sm.prefix( ).str( );
@@ -139,30 +145,24 @@ namespace daw {
 		struct ends_with_t {
 			ends_with_t( ) noexcept{ };
 			~ends_with_t( ) = default;
-			ends_with_t( const ends_with_t& ) noexcept{ };
+			ends_with_t( ends_with_t const & ) noexcept{ };
 			ends_with_t( ends_with_t&& ) noexcept{ }
 			ends_with_t& operator=(ends_with_t) const noexcept{ return *this; }
-			bool operator==(const ends_with_t&) const noexcept{ return true; }
-			bool operator<(const ends_with_t&) const noexcept{ return false; }
+			bool operator==(ends_with_t const &) const noexcept{ return true; }
+			bool operator<(ends_with_t const &) const noexcept{ return false; }
 
-				bool operator()( const StringType& src, const char ending ) const noexcept{
+				bool operator()( StringType const & src, const char ending ) const noexcept{
 				return 0 < src.size( ) && ending == src[src.size( ) - 1];
 			}
 
-				bool operator()( const StringType& src, const StringType& ending ) const noexcept{
+				bool operator()( StringType const & src, StringType const & ending ) const noexcept{
 				auto pos = src.find_last_of( ending );
 				return details::string_t::npos != pos && pos == src.size( ) - 1;
 			}
 		};
 
-		template<typename StringType, typename Ending>
-		bool ends_with( const StringType& src, const Ending& ending ) {
-			const static auto func = ends_with_t<StringType>( );
-			return func( src, ending );
-		}
-
 		template<typename StringType>
-		StringType trim_right_copy( const StringType& s, const StringType& delimiters = StringType( " \f\n\r\t\v\0" ) ) {
+		StringType trim_right_copy( StringType const & s, StringType const & delimiters = StringType( " \f\n\r\t\v\0" ) ) {
 			if( 0 == s.size( ) ) {
 				return s;
 			}
@@ -170,7 +170,7 @@ namespace daw {
 		}
 
 		template<typename StringType>
-		StringType trim_left_copy( const StringType& s, const StringType& delimiters = StringType( " \f\n\r\t\v\0" ) ) {
+		StringType trim_left_copy( StringType const & s, StringType const & delimiters = StringType( " \f\n\r\t\v\0" ) ) {
 			if( 0 == s.size( ) ) {
 				return s;
 			}
@@ -178,30 +178,30 @@ namespace daw {
 		}
 
 		template<class StringElementType, class StringType>
-		bool in( const StringElementType& val, const StringType& values ) {
+		bool in( StringElementType const & val, StringType const & values ) {
 			return StringType::npos != values.find( val );
 		}
 
 		template<typename StringType>
-		void trim_nocopy( StringType& str, const StringType& chars = " \f\n\r\t\v\0" ) {
-			const auto start = str.find_first_not_of( chars );
+		void trim_nocopy( StringType& str, StringType const & chars = " \f\n\r\t\v\0" ) {
+			auto const start = str.find_first_not_of( chars );
 			if( StringType::npos == start ) {
 				str.clear( );
 				return;
 			}
-			const auto end = str.find_last_not_of( chars );
-			const auto len = end - start + 1;
+			auto const end = str.find_last_not_of( chars );
+			auto const len = end - start + 1;
 			str = str.substr( start, len );
 		}
 
 		template<typename StringType>
-		StringType trim_copy( StringType s, const StringType& delimiters = " \f\n\r\t\v\0" ) {
+		StringType trim_copy( StringType s, StringType const & delimiters = " \f\n\r\t\v\0" ) {
 			trim_nocopy( s, delimiters );
 			return s;
 		}
 
 		template<typename StringType>
-		bool conertTo( const StringType& from, int& to ) {
+		bool conertTo( StringType const & from, int& to ) {
 			auto it = from.begin( );
 			using namespace boost::spirit;
 			if( !qi::parse( it, from.end( ), qi::int_, to ) ) {
@@ -211,7 +211,7 @@ namespace daw {
 		}
 
 		template<typename StringType>
-		bool convertTo( const StringType& from, int64_t& to ) {
+		bool convertTo( StringType const & from, int64_t& to ) {
 			auto it = from.begin( );
 			using namespace boost::spirit;
 			if( !qi::parse( it, from.end( ), qi::long_long, to ) ) {
@@ -221,7 +221,7 @@ namespace daw {
 		}
 
 		template<typename StringType>
-		bool convertTo( const StringType& from, float& to ) {
+		bool convertTo( StringType const & from, float& to ) {
 			auto it = from.begin( );
 			using namespace boost::spirit;
 			if( !qi::parse( it, from.end( ), qi::float_, to ) ) {
@@ -231,7 +231,7 @@ namespace daw {
 		}
 
 		template<typename StringType>
-		bool convertTo( const StringType& from, double& to ) {
+		bool convertTo( StringType const & from, double& to ) {
 			auto it = from.begin( );
 			using namespace boost::spirit;
 			if( !qi::parse( it, from.end( ), qi::double_, to ) ) {
@@ -241,17 +241,17 @@ namespace daw {
 		}
 
 		template<class T, class U>
-		std::string convertToString( const std::pair<T, U>& from ) {
+		std::string convertToString( std::pair<T, U> const & from ) {
 			return string_join( "{", to_string( from.first ), ", ", to_string( from.second ), " }" );
 		}
 
 		template<class ValueType>
-		void convertToString( const ValueType& from, details::string_t& to ) {
+		void convertToString( ValueType const & from, details::string_t& to ) {
 			to = to_string( from );
 		}
 
 		template<class ValueType>
-		void convertToString( const ValueType& from, details::string_t& to, const details::string_t& locale_str ) {
+		void convertToString( ValueType const & from, details::string_t& to, details::string_t const & locale_str ) {
 #pragma message( "Use non-locale version" )
 			static std::stringstream ss;
 			clear( ss );
