4eca0ced5316705f67659f64638775daf4c476c2
Darrell Wright <dwright-git@dawdevel.ca>
2014-11-30 01:23:15 -0500

    Fixing build errors

--- base_error.cpp
+++ base_error.cpp
@@ -1,49 +1,55 @@
-first
-first
-first
 #include <boost/lexical_cast.hpp>
 #include <boost/system/error_code.hpp>
 #include <stdexcept>
 #include <string>
 #include "base_error.h"
-#include "make_unique.h"
 
 namespace daw {
 	namespace nodepp {
 		namespace base {
-			Error::Error( std::string description ) : m_keyvalues{ }, m_frozen{ false } { 
+			Error::Error( std::string description ) : m_keyvalues{ }, m_frozen{ false }, m_child{ } { 
 				m_keyvalues.emplace( "description", description );
 			}
 
-			Error::Error( boost::system::error_code const & err ) : m_keyvalues{ }, m_frozen{ false } {
+			Error::Error( boost::system::error_code const & err ) : m_keyvalues{ }, m_frozen{ false }, m_child{ } {
 				m_keyvalues.emplace( "description", err.message( ) );
 				m_keyvalues.emplace( "category", boost::lexical_cast<std::string>(err.category( ).name( )) );
 				m_keyvalues.emplace( "error_code", boost::lexical_cast<std::string>(err.value( )) );
  			}
 
-			Error::Error( Error && other ) : m_keyvalues( std::move( other.m_keyvalues ) ), m_frozen{ std::move( other.m_frozen ) } { }
+			Error::Error( Error && other ) : m_keyvalues( std::move( other.m_keyvalues ) ), m_frozen{ std::move( other.m_frozen ) }, m_child{ std::move( other.m_child ) } { }
 
 			Error& Error::operator=(Error && rhs) {
 				if( this != &rhs ) {
 					m_keyvalues = std::move( rhs.m_keyvalues );
 					m_frozen = std::move( rhs.m_frozen );
+					m_child = std::move( rhs.m_child );
 				}
 				return *this;
 			}
 
-			Error& Error::add( std::string name, std::string value ) {
+			Error& Error::add( std::string const & name, std::string value ) {
 				if( m_frozen ) {
 					throw std::runtime_error( "Attempt to change a frozen error." );
 				}
-				m_keyvalues.emplace( std::move( name ), std::move( value ) );
+				m_keyvalues[name] = std::move( value );
 				return *this;
+			}
+
+
+			std::string const & Error::get( std::string const & name ) const {
+				return m_keyvalues.at( name );
+			}
+
+			std::string & Error::get( std::string const & name ) {
+				return m_keyvalues[name];
 			}
 
 			void Error::freeze( ) {
 				m_frozen = true;
 			}
 
-			Error const & Error::get_child( ) const {
+			Error & Error::get_child( ) const {
 				return *m_child.get( );
 			}
 
@@ -60,21 +66,21 @@ namespace daw {
 			}
 
 			Error& Error::set_child( Error const & child ) {
-				m_child = daw::make_unique<Error>( child );
+				m_child = std::make_shared<Error>( child );
 				m_child->freeze( );
 				return *this;
 			}
 
-			std::string Error::to_string( std::string prefix = "" ) const {
+			std::string Error::to_string( std::string prefix ) const {
 				std::stringstream ss;
-				ss << "Description: " << m_keyvalues["description"] << "\n";
-				for( auto & const row : m_keyvalues ) {
+				ss << "Description: " << m_keyvalues.at( "description" ) << "\n";
+				for( auto const & row : m_keyvalues ) {
 					if( row.first.compare( "description" ) != 0 ) {
 						ss << prefix << "'" << row.first << "',	'" << row.second << "'\n";
 					}
 				}
-				if( m_child ) {
-					ss << m_child->to_string( prefix + "	" );
+				if( has_child( ) ) {
+					ss << get_child( ).to_string( prefix + "	" );
 				}
 				ss << "\n";
 				return ss.str( );
--- base_error.h
+++ base_error.h
@@ -2,9 +2,9 @@
 
 #include <boost/system/error_code.hpp>
 #include <iostream>
+#include <map>
 #include <memory>
 #include <string>
-#include <unordered_map>
 
 namespace daw {
 	namespace nodepp {
@@ -14,26 +14,28 @@ namespace daw {
 			//				Description is mandatory.
 			// Requires:
 			class Error {
-				std::unordered_map<std::string, std::string> m_keyvalues;
-				std::unique_ptr<Error> m_child;
+				std::map<std::string, std::string> m_keyvalues;
 				bool m_frozen;
+				std::shared_ptr<Error> m_child;
 			public:
 				Error( ) = delete;
 				Error( Error const & ) = default;
 				Error& operator=(Error const &) = default;
 
 				Error( std::string description );
-				Error( boost::system::error_code const & err );
+				explicit Error( boost::system::error_code const & err );
 				Error( Error && );
 				Error& operator=(Error &&);
-				Error& add( std::string name, std::string value );
-				Error const &  get_child( ) const;
+				Error& add( std::string const & name, std::string value );
+				std::string const & get( std::string const & name ) const;
+				std::string & get( std::string const & name );
+				Error & get_child( ) const;
 				bool has_child( ) const;
 				Error& clear_child( );
 				Error& set_child( Error const & child );
 				void freeze( );
 
-				std::string to_string( ) const;
+				std::string to_string( std::string prefix = "" ) const;
 			};	// class Error
 			
 			std::ostream& operator<<(std::ostream& os, Error const & error);
