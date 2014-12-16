d1dfd840e003e55c3d2b89640f22176775c44d25
Darrell Wright <dwright-git@dawdevel.ca>
2014-12-15 21:43:45 -0500

    .

--- lib_http_server_response.cpp
+++ lib_http_server_response.cpp
@@ -1,5 +1,4 @@
-#include <boost/date_time/c_local_time_adjustor.hpp>
-#include <boost/date_time/local_time_adjustor.hpp>
+#include <boost/date_time.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <cstdint>
 #include <string>
