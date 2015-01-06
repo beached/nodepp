cde807c25af1aed0a17801d6035a53cd0cf9beab
Darrell Wright <dwright-git@dawdevel.ca>
2015-01-06 02:59:06 -0500

    Reverting to last working

--- base_json.h
+++ base_json.h
@@ -1,29 +1,5 @@
 #pragma once
 
-//	The MIT License (MIT)
-//	
-//	Copyright (c) 2014-2015 Darrell Wright
-//	
-//	Permission is hereby granted, free of charge, to any person obtaining a copy
-//	of this software and associated documentation files (the "Software"), to deal
-//	in the Software without restriction, including without limitation the rights
-//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//	copies of the Software, and to permit persons to whom the Software is
-//	furnished to do so, subject to the following conditions:
-//	
-//	The above copyright notice and this permission notice shall be included in all
-//	copies or substantial portions of the Software.
-//	
-//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//	SOFTWARE.
-//
-
-
 #include <boost/optional.hpp>
 #include <ctime>
 #include <cstdint>
@@ -49,8 +25,6 @@ namespace daw {
 				namespace details {
 					std::string json_name( std::string const & name );
 					std::string enbracket( std::string const & json_value );
-
-					GENERATE_HAS_MEMBER( cbegin, T::const_iterator );
 				}	// namespace details
 
 				// string
@@ -87,8 +61,8 @@ namespace daw {
 				void json_to_value( std::string const & json_text, boost::optional<T> & value );
 
 				// array.
-// 				template<typename T, typename C = dtraits::enable_if_t<details::has_cbegin<T>::value>>
-// 				std::string value_to_json( std::string const & name, T const & values );
+				template<typename T, typename C = dtraits::enable_if_t<dtraits::is_container<T>::value>>
+				std::string value_to_json( std::string const & name, T const & values );
 
 				// Definitions
 				// Numbers
@@ -119,7 +93,7 @@ namespace daw {
 				}
 
 				// array.
-				template<typename T, typename C = dtraits::enable_if_t<details::has_cbegin<T>::value>>
+				template<typename T, typename C>
 				std::string value_to_json( std::string const & name, T const & values ) {
 				std::stringstream result;
 					result << details::json_name( name ) + "[\n";
