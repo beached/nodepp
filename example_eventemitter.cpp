4bef3f50c919efce7efd233442461afed451a06c
Darrell Wright <dwright-git@dawdevel.ca>
2015-10-30 21:46:42 -0400

    EventEmitter example highlights chain of events

--- example_eventemitter.cpp
+++ example_eventemitter.cpp
@@ -21,20 +21,51 @@
 // SOFTWARE.
 
 #include <cstdlib>
+#include <iostream>
+#include "base_event_emitter.h"
+#include <future>
+#include <cmath>
+#include <thread>
 
-#include "base_eventemitter.h"
+template<typename ValueType, typename Function>
+void sequence( ValueType const & low, ValueType const & high, ValueType inc, Function op ) {
+	assert( low <= high );
+	ValueType n = low;
+	while( n <= high ) {
+		op( n );
+		n += inc;
+	}
+}
+
+constexpr double PI( ) {
+	return 3.141592653589793238463;
+}
 
 int main( int, char const ** ) {
 	using namespace daw::nodepp;
 
-	
 	auto event_emitter = base::create_event_emitter( );
 
-	
+	event_emitter->on( "calc_area", [event_emitter]( double r ) {
+		event_emitter->emit( "conv_sqft_to_sqm", r*r*PI( ) );
+	} );
 
+	event_emitter->on( "conv_sqft_to_sqm", [event_emitter]( double A ) {
+		event_emitter->emit( "done", A*0.092903 );
+	} );
 
+	event_emitter->on( "done", []( double Asqm ) {
+		std::cout << Asqm << std::endl;
+	} );
 
+	auto srv = std::async( [event_emitter]( ) {
+		base::start_service( base::StartServiceMode::Single );
+	} );
 
-	base::start_service( base::StartServiceMode::Single );
+	sequence( 1.0, 1000.0, 1.0, [event_emitter]( double val ) {
+		event_emitter->emit( "calc_area", val );
+	} );
+
+	srv.wait( );
 	return EXIT_SUCCESS;
 }
