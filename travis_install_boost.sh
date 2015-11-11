046fceec96f9069ca1a018fceca3e1514e33e3a8
Darrell Wright <dwright-git@dawdevel.ca>
2015-11-11 01:19:44 -0500

    added custom boost jam for g++-5

--- travis_install_boost.sh
+++ travis_install_boost.sh
@@ -4,11 +4,11 @@ set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$BOOST_ROOT" ]; then
 	echo "building boost"
-	alias gcc='${CC}';
-	alias g++='${CXX}';
 	wget -O boost_1_59_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download;
 	tar -xjf boost_1_59_0.tar.bz2;
-	cd boost_1_59_0 && ./bootstrap.sh --prefix="$BOOST_ROOT" && ./b2 -d0 -q install; 
+	cd boost_1_59_0;
+	echo "using gcc : : $BOOST_CXX ;" > ~/user-config.jam
+	./bootstrap.sh --prefix="$BOOST_ROOT" && ./b2 install; 
 else
 	echo 'Using cached boost directory.';
 fi
