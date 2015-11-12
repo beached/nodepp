e73e2001b0bdbe1b8c28ef808c0afc241ce63ff5
Darrell Wright <dwright-git@dawdevel.ca>
2015-11-12 14:55:49 -0500

    Moved check to whole file

--- travis_install_boost.sh
+++ travis_install_boost.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-# check to see if protobuf folder is empty
-if [ ! -d ~/boost.git ]; then
-	git clone --recursive https://github.com/boostorg/boost.git ~/boost.git;
-	cd ~/boost.git
-	git checkout boost-1.59.0
-fi
-
-
 if [ ! -d $BOOST_ROOT ]; then
+	# check to see if protobuf folder is empty
+	if [ ! -d ~/boost.git ]; then
+		git clone --recursive https://github.com/boostorg/boost.git ~/boost.git;
+		cd ~/boost.git
+		git checkout boost-1.59.0
+	fi
+
 	echo "building boost";
 	cd ~/boost.git;
 
 	echo "using gcc : : $BOOST_CXX ;" > ~/user-config.jam;
 	./bootstrap.sh --prefix="$BOOST_ROOT" && ./b2 --prefix="$BOOST_ROOT" && ./b2 --prefix="$BOOST_ROOT" headers && ./b2 --prefix="$BOOST_ROOT" install; 
+else
+	echo "using cached boost folder"
 fi
 
