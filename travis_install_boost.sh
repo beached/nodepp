86cdbd1efe607f22577ee8cd16a763412cc35b7b
Darrell Wright <dwright-git@dawdevel.ca>
2015-11-12 14:52:57 -0500

    Added check if BOOST_ROOT exists to skip building

--- travis_install_boost.sh
+++ travis_install_boost.sh
@@ -8,8 +8,12 @@ if [ ! -d ~/boost.git ]; then
 	git checkout boost-1.59.0
 fi
 
-echo "building boost";
-cd ~/boost.git;
 
-echo "using gcc : : $BOOST_CXX ;" > ~/user-config.jam;
-./bootstrap.sh --prefix="$BOOST_ROOT" && ./b2 --prefix="$BOOST_ROOT" && ./b2 --prefix="$BOOST_ROOT" headers && ./b2 --prefix="$BOOST_ROOT" install; 
+if [ ! -d $BOOST_ROOT ]; then
+	echo "building boost";
+	cd ~/boost.git;
+
+	echo "using gcc : : $BOOST_CXX ;" > ~/user-config.jam;
+	./bootstrap.sh --prefix="$BOOST_ROOT" && ./b2 --prefix="$BOOST_ROOT" && ./b2 --prefix="$BOOST_ROOT" headers && ./b2 --prefix="$BOOST_ROOT" install; 
+fi
+
