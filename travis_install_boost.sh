0daa9570592cad556525ba3904efd01c4d264b10
Darrell Wright <dwright-git@dawdevel.ca>
2015-11-13 21:47:55 -0500

    Fixed CMakeLists.txt for building unit tests as it was not adding link directory for Boost Turned off warning of depreciated stuff as std::auto_ptr is all over boost

--- travis_install_boost.sh
+++ travis_install_boost.sh
@@ -4,18 +4,17 @@ set -e
 if [ ! -d $BOOST_ROOT ]; then
 	# check to see if protobuf folder is empty
 	if [ ! -d ~/boost.git ]; then
-		git clone --recursive https://github.com/boostorg/boost.git ~/boost.git;
-		cd ~/boost.git
-		git checkout boost-1.59.0
+		wget -O boost_1_59_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download
+		tar -xjf boost_1_59_0.tar.bz2
 	fi
 
 	echo "building boost";
-	cd ~/boost.git;
+	cd boost_1_59_0;
 
 	echo "using gcc : : $BOOST_CXX ;" > ~/user-config.jam;
-	./bootstrap.sh
+	./bootstrap.sh --prefix="$BOOST_ROT"
 	
-	./b2 --prefix="$BOOST_ROOT" headers install; 
+	./b2 --prefix="$BOOST_ROOT" install; 
 else
 	echo "using cached boost folder"
 fi
