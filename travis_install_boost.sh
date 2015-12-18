e105da62c53174da9494dbe1903ca0874e50f658
Darrell Wright <dwright-git@dawdevel.ca>
2015-12-17 23:29:24 -0500

    Updated travis_install_boost.sh to pull boost 1.60

--- travis_install_boost.sh
+++ travis_install_boost.sh
@@ -4,12 +4,12 @@ set -e
 if [ ! -d $BOOST_ROOT ]; then
 	# check to see if protobuf folder is empty
 	if [ ! -d ~/boost.git ]; then
-		wget -O boost_1_59_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download
-		tar -xjf boost_1_59_0.tar.bz2
+		wget -O boost_1_60_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.bz2/download
+		tar -xjf boost_1_60_0.tar.bz2
 	fi
 
 	echo "building boost";
-	cd boost_1_59_0;
+	cd boost_1_60_0;
 
 	echo "using gcc : : $BOOST_CXX ;" > ~/user-config.jam;
 	./bootstrap.sh --prefix="$BOOST_ROOT"
