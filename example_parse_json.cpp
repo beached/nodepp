4c248ce7eb5615cce5e6ec19e8d5b94152a4d198
Darrell Wright <dwright-git@dawdevel.ca>
2015-12-29 01:48:36 -0500

    Started a parse_json example.  Needs to be non-posix only.

--- example_parse_json.cpp
+++ example_parse_json.cpp
@@ -20,11 +20,63 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include "parse_json/daw_json.h"
+#include "parse_json/daw_json_parser.h"
 #include <cstdlib>
+#include <sys/mman.h>
+#include <cstdio>
+#include <unistd.h>
+#include <err.h>
+#include <fcntl.h>
+#include <iostream>
 
-int main( int, char** ) {
+int main( int argc, char** argv ) {
+	assert( argc > 1 || "Must supply a json file" );
+	int fd = -1;
+	if( (fd = open( argv[1], O_RDONLY, 0 )) == -1 ) {
+		err( 1, "open" );
+	}
 
+	char const * const json_str = static_cast<char*>( mmap( nullptr, 4096, PROT_READ, MAP_FILE|MAP_PRIVATE, fd, 0 ) );
+
+	if( MAP_FAILED == json_str ) {
+		errx( 1, "mmap" );
+	}
+
+
+	using namespace daw::json::impl;
+	using namespace daw::json;
+
+	std::cout << json_str << std::endl;
+
+	auto json = parse_json( json_str );
+	if( !json ) {
+		std::cerr << "Could not find data" << std::endl;
+		exit( EXIT_FAILURE );
+	}
+
+	std::cout << "value: " << daw::json::get<std::string>( *json ) << "\n";
+	switch( json->type( ) ) {
+	case value_t::value_types::integral:
+		std::cout << json->get_integral( ) << "\n";
+		break;
+	case value_t::value_types::real:
+		std::cout << json->get_real( ) << "\n";
+		break;
+	case value_t::value_types::boolean:
+		std::cout << json->get_boolean( ) << "\n";
+		break;
+	case value_t::value_types::string:
+		std::cout << "'" << json->get_string( ) << "'\n";
+		break;
+	case value_t::value_types::null:
+		std::cout << "null\n";
+		break;
+	default:
+		std::cout << "other\n";
+	}	
+
+	munmap( (void*)json_str, 4096 );
+	close( fd );
 
 	return EXIT_SUCCESS;
 }
