4f56e9dedb925bffac5072f6858adff606eaccbc
Darrell Wright <dwright-git@dawdevel.ca>
2016-02-15 20:06:38 -0500

    .

--- example_parse_json.cpp
+++ example_parse_json.cpp
@@ -50,7 +50,7 @@ int main( int argc, char** argv ) {
 		exit( EXIT_FAILURE );
 	}
 	
-	//std::cout << "value: " << json <<"\n";
+//	std::cout << "value: " << json <<"\n";
 	// system( "pause" );
 	return EXIT_SUCCESS;
 }
